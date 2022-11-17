e6ead34c64dbec6b92c4c9d0539dec9f77fcd86c
mangopanda455 <gfielding26@scarsdaleschools.org>
2022-11-17 10:39:58 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    //write a program that camel cases a string
+    String str = "hello world";
+    String[] words = str.split(" ");
+    String camelCase = "";
+    for (int i = 0; i < words.length; i++) {
+      String word = words[i];
+      String firstLetter = word.substring(0, 1);
+      String restOfWord = word.substring(1);
+      camelCase += firstLetter.toUpperCase() + restOfWord;
+    }
+    System.out.println(camelCase);
+  }
+}
